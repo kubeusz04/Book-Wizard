cfc4d95a538b22af40840cfec0a534352209553d
Zawadzki Kuba <58346333+kubeusz04@users.noreply.github.com>
2025-06-11 22:31:39 +0200

    Delete src directory

--- src/main/java/com/example/bookapp/repository/PlotPlanRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.bookapp.repository;
-
-import com.example.bookapp.model.PlotPlan;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.Optional;
-
-public interface PlotPlanRepository extends JpaRepository<PlotPlan, Long> {
-    Optional<PlotPlan> findByBookId(Long bookId);
-}
